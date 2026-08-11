f4d78a43eeff353b8036dd11540f13b1b0bc1fda
Devaki-05 <devaki530497@gmail.com>
2026-08-11 16:14:12 +0530

    Create 619-BiggestSingleNumber

---
+++ 619-BiggestSingleNumber/619-BiggestSingleNumber.sql
@@ -0,0 +1,9 @@
+-- Last updated: 11/08/2026, 16:14:12
+# Write your MySQL query statement below
+SELECT MAX(num) AS num
+FROM (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+) AS unique_numbers;
