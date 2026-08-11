cddfa92dd9dd2d9c8ebac03bdf83fc32de924fea
Devaki-05 <devaki530497@gmail.com>
2026-08-11 16:17:12 +0530

    Create 237-DeleteNodeInALinkedList

---
+++ 237-DeleteNodeInALinkedList/237-DeleteNodeInALinkedList.java
@@ -0,0 +1,18 @@
+// Last updated: 11/08/2026, 16:17:12
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val =node.next.val;
+        node.next = node.next.next;
+        
+
+        
+    }
+}
