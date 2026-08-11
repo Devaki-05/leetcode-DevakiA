996b0811665b0ef310cbfd31a02506ab851a10d1
Devaki-05 <devaki530497@gmail.com>
2026-08-11 16:13:46 +0530

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.java
@@ -0,0 +1,22 @@
+// Last updated: 11/08/2026, 16:13:46
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0;
+        int y = 0;
+        
+        
+        for (char move : moves.toCharArray()) {
+            if (move == 'U') {
+                y++;
+            } else if (move == 'D') {
+                y--;
+            } else if (move == 'R') {
+                x++;
+            } else if (move == 'L') {
+                x--;
+            }
+        }
+        
+        return x == 0 && y == 0;
+    }
+}
