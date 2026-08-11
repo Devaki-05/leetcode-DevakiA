3e37d6d4c8865d18f7840fba4e6723e3aab40a9e
Devaki-05 <devaki530497@gmail.com>
2026-08-11 16:19:49 +0530

    Create 71-SimplifyPath

---
+++ 71-SimplifyPath/71-SimplifyPath.java
@@ -0,0 +1,24 @@
+// Last updated: 11/08/2026, 16:19:49
+class Solution {
+    public String simplifyPath(String path) {
+        String[]arr =path.split("/");
+        Stack<String> st = new Stack<>();
+        for(String ele :arr){
+            if(ele.equals("") || ele.equals("."))
+            continue;
+        else if(ele.equals("..")){
+        if(!st.isEmpty())
+            st.pop();}
+        else
+        st.push(ele);
+        }
+        if(st.isEmpty())
+        return"/";
+        StringBuilder sb = new StringBuilder();
+        for(String ele :st){
+            sb.append("/");
+            sb.append(ele);
+        }
+        return sb.toString();
+    }
+}
