372c32d5c695ed988954b6e742800a886f405d7a
Devaki-05 <devaki530497@gmail.com>
2026-08-11 16:20:56 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,32 @@
+// Last updated: 11/08/2026, 16:20:56
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+      
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else {
+               
+                if (stack.isEmpty() || stack.pop() != c) {
+                    return false;
+                }
+            }
+        }
+
+      
+        return stack.isEmpty();
+    }
+}
