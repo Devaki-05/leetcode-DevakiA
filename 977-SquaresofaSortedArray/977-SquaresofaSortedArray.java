b3d8faa1454033cf2aa396f3294b6991704f5a49
Devaki-05 <devaki530497@gmail.com>
2026-08-24 09:21:51 +0530

    Create 977-SquaresofaSortedArray

---
+++ 977-SquaresofaSortedArray/977-SquaresofaSortedArray.java
@@ -0,0 +1,28 @@
+// Last updated: 24/08/2026, 09:21:52
+1class Solution {
+2    public int[] sortedSquares(int[] nums) {
+3        int n = nums.length;
+4        int[] result = new int[n];
+5        
+6        int left = 0;
+7        int right = n - 1;
+8        int index = n - 1;
+9        
+10
+11        while (left <= right) {
+12            int leftSquare = nums[left] * nums[left];
+13            int rightSquare = nums[right] * nums[right];
+14            
+15            if (leftSquare > rightSquare) {
+16                result[index] = leftSquare;
+17                left++;
+18            } else {
+19                result[index] = rightSquare;
+20                right--;
+21            }
+22            index--;
+23        }
+24        
+25        return result;
+26    }
+27}
