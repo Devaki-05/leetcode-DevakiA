be5ad610adc05729b07e95e11d49924d4f93b5d6
Devaki-05 <devaki530497@gmail.com>
2026-08-31 09:37:16 +0530

    Create 637-AverageofLevelsinBinaryTree

---
+++ 637-AverageofLevelsinBinaryTree/637-AverageofLevelsinBinaryTree.java
@@ -0,0 +1,9 @@
+// Last updated: 31/08/2026, 09:37:16
+1class Solution {
+2    public int maxDepth(TreeNode root) {
+3        if (root == null) {
+4            return 0;
+5        }
+6        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+7    }
+8}
