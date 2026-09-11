48cbd6661760861ff8060fca5bd69ff3a6fb1c42
Devaki-05 <devaki530497@gmail.com>
2026-09-11 09:54:54 +0530

    File updated at 11/09/2026, 09:54:49

--- 543-DiameterofBinaryTree/543-DiameterofBinaryTree.java
+++ 543-DiameterofBinaryTree/543-DiameterofBinaryTree.java
@@ -1,28 +1,6 @@
-// Last updated: 11/09/2026, 09:23:09
-1/**
-2 * Definition for a binary tree node.
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode() {}
-8 *     TreeNode(int val) { this.val = val; }
-9 *     TreeNode(int val, TreeNode left, TreeNode right) {
-10 *         this.val = val;
-11 *         this.left = left;
-12 *         this.right = right;
-13 *     }
-14 * }
-15 */
-16class Solution {
-17    public TreeNode searchBST(TreeNode root, int val) {
-18        if(root == null || root.val ==val){
-19            return root;
-20        }
-21        if(val<root.val){
-22            return searchBST(root.left,val);
-23        }else{
-24            return searchBST(root.right,val);
-25        }
-26    }
-27}
+// Last updated: 11/09/2026, 09:54:49
+1class Solution {
+2    public int hammingDistance(int x, int y) {
+3        return Integer.bitCount(x^y);
+4    }
+5}
