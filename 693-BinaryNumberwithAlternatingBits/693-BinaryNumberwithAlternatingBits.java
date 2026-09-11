56ad98e9befc1f3ffa24a7edaacb2dae2a6cacf9
Devaki-05 <devaki530497@gmail.com>
2026-09-11 16:17:22 +0530

    File updated at 11/09/2026, 16:17:19

--- 693-BinaryNumberwithAlternatingBits/693-BinaryNumberwithAlternatingBits.java
+++ 693-BinaryNumberwithAlternatingBits/693-BinaryNumberwithAlternatingBits.java
@@ -1,47 +1,36 @@
-// Last updated: 11/09/2026, 16:11:41
-1import java.util.ArrayList;
-2import java.util.List;
-3
-4class Solution {
-5    public List<Integer> spiralOrder(int[][] matrix) {
-6        List<Integer> result = new ArrayList<>();
-7        if (matrix == null || matrix.length == 0) return result;
-8
-9        int top = 0;
-10        int bottom = matrix.length - 1;
-11        int left = 0;
-12        int right = matrix[0].length - 1;
-13
-14        while (top <= bottom && left <= right) {
-15            // Traverse Right
-16            for (int j = left; j <= right; j++) {
-17                result.add(matrix[top][j]);
-18            }
-19            top++;
-20
-21            // Traverse Down
-22            for (int i = top; i <= bottom; i++) {
-23                result.add(matrix[i][right]);
-24            }
-25            right--;
-26
-27            // Traverse Left
-28            if (top <= bottom) {
-29                for (int j = right; j >= left; j--) {
-30                    result.add(matrix[bottom][j]);
-31                }
-32                bottom--;
-33            }
-34
-35            // Traverse Up
-36            if (left <= right) {
-37                for (int i = bottom; i >= top; i--) {
-38                    result.add(matrix[i][left]);
-39                }
-40                left++;
-41            }
-42        }
-43
-44        return result;
-45    }
-46}
+// Last updated: 11/09/2026, 16:17:19
+1class Solution {
+2    public int[][] generateMatrix(int n) {
+3        int[][] matrix = new int[n][n];
+4        int num = 1;
+5        int top = 0, bottom = n - 1;
+6        int left = 0, right = n - 1;
+7
+8        while (num <= n * n) {
+9         
+10            for (int j = left; j <= right; j++) {
+11                matrix[top][j] = num++;
+12            }
+13            top++;
+14
+15            for (int i = top; i <= bottom; i++) {
+16                matrix[i][right] = num++;
+17            }
+18            right--;
+19
+20            
+21            for (int j = right; j >= left; j--) {
+22                matrix[bottom][j] = num++;
+23            }
+24            bottom--;
+25
+26            
+27            for (int i = bottom; i >= top; i--) {
+28                matrix[i][left] = num++;
+29            }
+30            left++;
+31        }
+32
+33        return matrix;
+34    }
+35}
