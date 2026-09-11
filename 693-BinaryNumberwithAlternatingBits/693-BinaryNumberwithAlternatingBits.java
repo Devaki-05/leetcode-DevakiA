206b887b84ade3ab70147dc95f3d9917304c690e
Devaki-05 <devaki530497@gmail.com>
2026-09-11 16:20:18 +0530

    File updated at 11/09/2026, 16:20:15

--- 693-BinaryNumberwithAlternatingBits/693-BinaryNumberwithAlternatingBits.java
+++ 693-BinaryNumberwithAlternatingBits/693-BinaryNumberwithAlternatingBits.java
@@ -1,36 +1,18 @@
-// Last updated: 11/09/2026, 16:17:19
+// Last updated: 11/09/2026, 16:20:15
 1class Solution {
-2    public int[][] generateMatrix(int n) {
-3        int[][] matrix = new int[n][n];
-4        int num = 1;
-5        int top = 0, bottom = n - 1;
-6        int left = 0, right = n - 1;
-7
-8        while (num <= n * n) {
-9         
-10            for (int j = left; j <= right; j++) {
-11                matrix[top][j] = num++;
-12            }
-13            top++;
-14
-15            for (int i = top; i <= bottom; i++) {
-16                matrix[i][right] = num++;
-17            }
-18            right--;
-19
-20            
-21            for (int j = right; j >= left; j--) {
-22                matrix[bottom][j] = num++;
-23            }
-24            bottom--;
-25
-26            
-27            for (int i = bottom; i >= top; i--) {
-28                matrix[i][left] = num++;
-29            }
-30            left++;
-31        }
-32
-33        return matrix;
-34    }
-35}
+2    public int[] productExceptSelf(int[] nums) {
+3        int n = nums.length;
+4        int[]res = new int[n];
+5
+6        res[0]=1;
+7        for(int i=1;i<n;i++){
+8            res[i]=res[i-1]*nums[i-1];
+9        }
+10        int suffix =1;
+11        for(int i =n-1;i>=0;i--){
+12            res[i]*=suffix;
+13            suffix *=nums[i];
+14        }
+15        return res;
+16    }
+17}
