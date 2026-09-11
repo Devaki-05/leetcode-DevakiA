a92d7479a8a901e7a102abd9445d0f0b5e68eb50
Devaki-05 <devaki530497@gmail.com>
2026-09-11 16:11:44 +0530

    File updated at 11/09/2026, 16:11:41

--- 693-BinaryNumberwithAlternatingBits/693-BinaryNumberwithAlternatingBits.java
+++ 693-BinaryNumberwithAlternatingBits/693-BinaryNumberwithAlternatingBits.java
@@ -1,44 +1,47 @@
-// Last updated: 11/09/2026, 16:09:51
+// Last updated: 11/09/2026, 16:11:41
 1import java.util.ArrayList;
-2import java.util.Collections;
-3import java.util.HashMap;
-4import java.util.List;
-5import java.util.Map;
-6
-7class Solution {
-8    public List<List<Integer>> findWinners(int[][] matches) {
-9        Map<Integer, Integer> lossCount = new HashMap<>();
-10
-11        for (int[] match : matches) {
-12            int winner = match[0];
-13            int loser = match[1];
-14
-15            lossCount.putIfAbsent(winner, 0);
-16            lossCount.put(loser, lossCount.getOrDefault(loser, 0) + 1);
-17        }
-18
-19        List<Integer> zeroLoss = new ArrayList<>();
-20        List<Integer> oneLoss = new ArrayList<>();
-21
-22        for (Map.Entry<Integer, Integer> entry : lossCount.entrySet()) {
-23            int player = entry.getKey();
-24            int losses = entry.getValue();
-25
-26            if (losses == 0) {
-27                zeroLoss.add(player);
-28            } else if (losses == 1) {
-29                oneLoss.add(player);
-30            }
-31        }
-32
-33        Collections.sort(zeroLoss);
-34        Collections.sort(oneLoss);
-35
-36        List<List<Integer>> result = new ArrayList<>();
-37        result.add(zeroLoss);
-38        result.add(oneLoss);
-39
-40        return result;
-41    }
-42}
+2import java.util.List;
+3
+4class Solution {
+5    public List<Integer> spiralOrder(int[][] matrix) {
+6        List<Integer> result = new ArrayList<>();
+7        if (matrix == null || matrix.length == 0) return result;
+8
+9        int top = 0;
+10        int bottom = matrix.length - 1;
+11        int left = 0;
+12        int right = matrix[0].length - 1;
+13
+14        while (top <= bottom && left <= right) {
+15            // Traverse Right
+16            for (int j = left; j <= right; j++) {
+17                result.add(matrix[top][j]);
+18            }
+19            top++;
+20
+21            // Traverse Down
+22            for (int i = top; i <= bottom; i++) {
+23                result.add(matrix[i][right]);
+24            }
+25            right--;
+26
+27            // Traverse Left
+28            if (top <= bottom) {
+29                for (int j = right; j >= left; j--) {
+30                    result.add(matrix[bottom][j]);
+31                }
+32                bottom--;
+33            }
+34
+35            // Traverse Up
+36            if (left <= right) {
+37                for (int i = bottom; i >= top; i--) {
+38                    result.add(matrix[i][left]);
+39                }
+40                left++;
+41            }
+42        }
 43
+44        return result;
+45    }
+46}
