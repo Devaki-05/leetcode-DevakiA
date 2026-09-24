af4063afdcc3d7d9fdeb76ad59b515a8ed0cca82
Devaki-05 <devaki530497@gmail.com>
2026-09-24 16:04:36 +0530

    File updated at 24/09/2026, 16:04:34

--- 36-ValidSudoku/36-ValidSudoku.java
+++ 36-ValidSudoku/36-ValidSudoku.java
@@ -1,39 +1,46 @@
-// Last updated: 24/09/2026, 16:02:55
-1import java.util.*;
-2
-3class Solution {
-4    public int[] topKFrequent(int[] nums, int k) {
-5        
-6        Map<Integer, Integer> countMap = new HashMap<>();
-7        for (int num : nums) {
-8            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+// Last updated: 24/09/2026, 16:04:34
+1class Solution {
+2    public int splitArray(int[] nums, int k) {
+3        int maxVal = 0;
+4        int sumVal = 0;
+5
+6        for (int num : nums) {
+7            maxVal = Math.max(maxVal, num);
+8            sumVal += num;
 9        }
 10
-11        
-12        List<Integer>[] buckets = new List[nums.length + 1];
-13        for (int key : countMap.keySet()) {
-14            int freq = countMap.get(key);
-15            if (buckets[freq] == null) {
-16                buckets[freq] = new ArrayList<>();
-17            }
-18            buckets[freq].add(key);
-19        }
-20
-21        
-22        int[] result = new int[k];
-23        int index = 0;
-24
-25        for (int i = buckets.length - 1; i >= 0 && index < k; i--) {
-26            if (buckets[i] != null) {
-27                for (int num : buckets[i]) {
-28                    result[index++] = num;
-29                    if (index == k) {
-30                        return result;
-31                    }
-32                }
-33            }
-34        }
-35
-36        return result;
-37    }
-38}
+11        int left = maxVal;
+12        int right = sumVal;
+13
+14        while (left < right) {
+15            int mid = left + (right - left) / 2;
+16
+17            if (canSplit(nums, k, mid)) {
+18                right = mid; 
+19            } else {
+20                left = mid + 1; 
+21            }
+22        }
+23
+24        return left;
+25    }
+26
+27    private boolean canSplit(int[] nums, int k, int maxSum) {
+28        int currentSum = 0;
+29        int count = 1; 
+30
+31        for (int num : nums) {
+32            if (currentSum + num > maxSum) {
+33                count++;
+34                currentSum = num;
+35                if (count > k) {
+36                    return false;
+37                }
+38            } else {
+39                currentSum += num;
+40            }
+41        }
+42
+43        return true;
+44    }
+45}
