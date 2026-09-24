3f3185d8c0b3e394f0d6b17683d309ca8cffe62f
Devaki-05 <devaki530497@gmail.com>
2026-09-24 13:32:54 +0530

    File updated at 24/09/2026, 13:32:52

--- 143-ReorderList/143-ReorderList.java
+++ 143-ReorderList/143-ReorderList.java
@@ -1,25 +1,22 @@
-// Last updated: 24/09/2026, 13:31:33
+// Last updated: 24/09/2026, 13:32:52
 1class Solution {
-2    public ListNode partition(ListNode head, int x) {
-3        ListNode beforeHead = new ListNode(0);
-4        ListNode before = beforeHead;
-5        ListNode afterHead = new ListNode(0);
-6        ListNode after = afterHead;
+2    public ListNode removeNthFromEnd(ListNode head, int n) {
+3        ListNode dummy = new ListNode(0);
+4        dummy.next = head;
+5        ListNode fast = dummy;
+6        ListNode slow = dummy;
 7
-8        while (head != null) {
-9            if (head.val < x) {
-10                before.next = head;
-11                before = before.next;
-12            } else {
-13                after.next = head;
-14                after = after.next;
-15            }
-16            head = head.next;
-17        }
+8        for (int i = 0; i <= n; i++) {
+9            fast = fast.next;
+10        }
+11
+12        while (fast != null) {
+13            fast = fast.next;
+14            slow = slow.next;
+15        }
+16
+17        slow.next = slow.next.next;
 18
-19        after.next = null;
-20        before.next = afterHead.next;
-21
-22        return beforeHead.next;
-23    }
-24}
+19        return dummy.next;
+20    }
+21}
